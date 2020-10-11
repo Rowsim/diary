cb0c02a995237ccfb86cad1d886a54b8fac5af37
rowsim <rowanq@live.co.uk>
2020-10-11 17:34:39 +0100

    Added top navigation

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { Navigation } from "../../components/navigation/navigation";
 
-const HomePage = () => <div>Home page loaded!</div>;
+const HomePage = () => (
+  <>
+    <Navigation pageTitle='home'></Navigation>
+    <div>Home page loaded!</div>
+  </>
+);
 
 export default HomePage;
